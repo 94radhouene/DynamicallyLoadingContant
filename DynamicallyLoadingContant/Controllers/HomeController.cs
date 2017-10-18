eccff29e15811f4dedfbdd4782cc99db636f52db
AcerPC <AcerPC@DESKTOP-DMLRVJT>
2017-10-19 00:29:14 +0200

    How to dynamcly loading with jquery and ajax

---
+++ DynamicallyLoadingContant/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DynamicallyLoadingContant.Models;
+
+namespace DynamicallyLoadingContant.Controllers
+{
+    public class HomeController : Controller
+    {
+        TestMvcEntities db = new TestMvcEntities();
+        public ActionResult Index()
+        {
+            return View(db.EmployeeInfoes.ToList());
+        }
+
+        public ActionResult EmployeeInfo(int id)
+        {
+            List<EmployeeInfo> EmpInfo = db.EmployeeInfoes.Where(x => x.EmployeeId == id).ToList();
+            return View(EmpInfo);
+        }
+    }
+}
